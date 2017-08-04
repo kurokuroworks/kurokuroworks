a2fdffc5ec0c63609e91074deddb853423bd0f4c
nanocloudx <nanocloudx@gmail.com>
2017-08-05 00:39:24 +0900

    fix header

--- components/sections/common/header.vue
+++ components/sections/common/header.vue
@@ -49,95 +49,103 @@
     + section {
       margin-top: 60px;
     }
-  }
-  header {
-    position: absolute;
-    z-index: 2;
-    background-color: #ffffff;
-    left: 0;
-    right: 0;
-    height: 60px;
-    box-shadow: 0 1px 1px 0 rgba(0,0,0,0.1);
-    .header-title {
-      float: left;
-      text-align: center;
-      transition-duration: .5s;
-      width: 100%;
-      background-color: #71bfb6;
-      img {
-        display: block;
-        height: 90px;
-        margin: -30px auto 0;
-        padding: 20px;
-      }
-    }
-    .header-description {
-      display: none;
-      float: left;
-      padding: 8px 15px;
-    }
-  }
-  nav {
-    position: relative;
-    z-index: 1;
-    transition-duration: .5s;
-    top: 60px;
-    left: 0;
-    right: 0;
-    height: 50px;
-    background-color: #ffffff;
-    box-shadow: 0 1px 1px 0 rgba(0,0,0,0.1);
-    ul {
-      display: flex;
-      justify-content: center;
-      li {
-        width: 25%;
-        max-width: 150px;
-        height: 50px;
-        justify-content: center;
-        &:last-child p {
-          border-right: none;
-        }
-        p {
-          border-right: 1px solid #dddddd;
-          text-align: center;
-          line-height: 30px;
-          margin: 10px 0;
-          justify-content: center;
-          overflow: hidden;
-        }
-      }
-    }
-  }
-  .is-fixed {
-    + section {
-      margin-top: 100px;
-    }
+
     header {
-      position: fixed;
-      top: 0;
-      overflow: hidden;
+      position: absolute;
+      z-index: 2;
+      background-color: #ffffff;
+      left: 0;
+      right: 0;
+      height: 60px;
+      box-shadow: 0 1px 1px 0 rgba(0,0,0,0.1);
       .header-title {
-        width: 300px;
-        background-color: #669fcb;
+        float: left;
+        text-align: center;
+        transition-duration: .5s;
+        width: 100%;
+        background-color: #71bfb6;
         img {
-          height: 60px;
-          margin-top: 0;
-          padding: 8px;
+          display: block;
+          height: 90px;
+          margin: -30px auto 0;
+          padding: 20px;
         }
       }
       .header-description {
-        display: block;
+        display: none;
+        float: left;
+        padding: 8px 15px;
       }
     }
     nav {
-      position: fixed;
-      top: 0;
+      position: relative;
+      z-index: 1;
+      transition-duration: .5s;
+      top: 60px;
+      left: 0;
+      right: 0;
+      height: 45px;
+      background-color: #ffffff;
+      box-shadow: 0 1px 1px 0 rgba(0,0,0,0.1);
+      ul {
+        display: flex;
+        justify-content: center;
+        li {
+          width: 25%;
+          max-width: 150px;
+          height: 45px;
+          justify-content: center;
+          transition-duration: .3s;
+          &:hover {
+            background-color: #dbb6ef;
+          }
+          &:last-child p {
+            border-right: none;
+          }
+          p {
+            border-right: 1px solid #dddddd;
+            text-align: center;
+            line-height: 35px;
+            margin: 5px 0;
+            justify-content: center;
+            overflow: hidden;
+          }
+        }
+      }
     }
-  }
-  .is-visible-nav {
-    nav {
-      top: 59px; // box-shadow の見た目的に -1px
+    &.is-fixed {
+      + section {
+        margin-top: 100px;
+      }
+      header {
+        position: fixed;
+        top: 0;
+        overflow: hidden;
+        .header-title {
+          width: 300px;
+          background-color: #669fcb;
+          img {
+            height: 60px;
+            margin-top: 0;
+            padding: 8px;
+          }
+          @media (max-width:800px) {
+            width: 100%;
+          }
+        }
+        .header-description {
+          display: block;
+        }
+      }
+      nav {
+        position: fixed;
+        top: 0;
+      }
+    }
+    &.is-visible-nav {
+      nav {
+        top: 59px; // box-shadow の見た目的に -1px
+      }
     }
   }
 </style>
