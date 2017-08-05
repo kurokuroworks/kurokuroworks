ca17580f6cf3b9110753573fe84a3ef472db7d09
nanocloudx <nanocloudx@gmail.com>
2017-08-06 05:30:31 +0900

    fix design

--- components/sections/index/articles.vue
+++ components/sections/index/articles.vue
@@ -3,6 +3,23 @@
     <div class="container">
       <h2 class="section-title">Articles<span>記事・ブログ</span></h2>
       <ul class="item-container">
+        <item-parts
+          :type="'pickup'"
+          :id="'example'"
+          :href="'example'"
+          :thumbnail="'https://static.kurokuroworks.net/www/dummy/dummy_16x9.jpg'"
+          :title="'example'"
+          :meta="'example'"
+          :description="'example'"
+        ></item-parts>
+        <li class="side-container">
+          <div class="ads">
+            <p>middle rectangle ads 300x250</p>
+          </div>
+          <div class="ads">
+            <p>middle rectangle ads 300x250</p>
+          </div>
+        </li>
         <template v-for="item in data">
           <item-parts
             :type="'article'"
@@ -47,6 +64,25 @@
           background-color: #ffffff;
         }
         @media screen and (min-width: 735px) { // 720 + 15
+          width: 360px;
+          &.pickup {
+            width: 720px;
+          }
+        }
+      }
+      .side-container {
+        width: 100%;
+        padding: 10px 0 0;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        .ads {
+          width: 300px;
+          height: 250px;
+          background-color: #333234;
+          margin: 0 10px 10px;
+        }
+        @media (min-width:1095px) {
           width: 360px;
         }
       }
